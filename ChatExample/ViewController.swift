1cf085fc32a938bd42ffc5fe2d70d955ebd3457a
demothreen <demonnn@list.ru>
2022-04-27 00:16:42 +0500

    add swiftui canvas

--- ChatExample/ViewController.swift
+++ ChatExample/ViewController.swift
@@ -17,3 +17,20 @@ class ViewController: UIViewController {
 
 }
 
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+  static var previews: some View {
+    ContainerView().edgesIgnoringSafeArea(.all)
+  }
+
+  struct ContainerView: UIViewControllerRepresentable {
+    let viewController = ViewController()
+    func makeUIViewController(context: Context) -> some UIViewController {
+      return viewController
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+  }
+}
