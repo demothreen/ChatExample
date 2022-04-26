4669d6ba9306f8c99d17a93df8b5af704de77eec
demothreen <demonnn@list.ru>
2022-04-27 00:03:51 +0500

    Initial Commit

---
+++ ChatExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatExample
+//
+//  Created by demothreen on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red.withAlphaComponent(0.3)
+  }
+
+
+}
+
